705fcc0c74bfec72cab3e9963292fc9a35f30d0d
Wiser_code <wisdomwise943@gmail.com>
2026-04-15 21:51:18 +0100

    Update index.js

--- cpanel/index.js
+++ cpanel/index.js
@@ -2219,7 +2219,7 @@ window.openBindBankModal = async () => {
       </div>
       <div id="korapaySettings" style="display:none">
       
-      <div class="section-body" style="display:flex;">
+      <div class="section-body" style="display:block;">
               <div class="form-row">
                 <div class="form-group">
                   <label class="form-label">Payment Gateway</label>
