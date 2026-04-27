5e2e584a71c132551edad5a2a409abd05c6ce4e9
wiser943 <wisdomwise943@gmail.com>
2026-04-27 18:07:41 +0100

    update

--- m1/script.js
+++ m1/script.js
@@ -1276,9 +1276,17 @@ async function updateVerificationUI() {
             <div id="verificationIcon" class="stat-icon purple"><i class="ri-shield-check-line"></i></div>
             <div class="stat-value">Status</div>
             <div class="stat-label" id="verificationText">Checking...</div>
-            <div class="stat-change up"><i class="ri-arrow-up-s-line"></i>Type: Fex</div></div>
-
-`;
+            <div class="stat-change up"><i class="ri-arrow-up-s-line"></i>Type: Fex</div></div>`;
+  //to be added later
+  /*          <div style="font-size:12px;color:var(--text3);margin-bottom:6px;font-weight:600;letter-spacing:0.5px;">ACCOUNT LEVEL</div>
+            <div style="font-weight:700;color:var(--accent2);font-size:18px;font-family:'Syne',sans-serif;display:flex;align-items:center;gap:6px;">
+              <i class="ri-medal-line"></i>Gold Tier
+            </div>
+            <div class="progress-bar mt-4">
+              <div class="progress-fill purple" style="width:74%"></div>
+            </div>
+            <div class="text-xs mt-4">₦26,000 to Platinum</div>
+            */
   
   const text = document.getElementById('verificationText');
   const label = document.getElementById('verificationLabel');
